9c02b45a9118d6b1a76c5a15aaf68f18a71b4af4
praneethb7 <praneethbudati.work@gmail.com>
2026-02-21 02:18:15 +0530

    blah2

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -1,5 +1,7 @@
 import EventDetails from "@/components/EventDetails";
 
+export const dynamic = "force-dynamic";
+
 const EventDetailsPage = ({ params }: { params: { slug: string } }) => {
     return (
         <main>
